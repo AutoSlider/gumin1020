7f961ec5b7da144128975b099bcc8e70b810656e
gumin1020 <nsa06194@naver.com>
2023-03-31 16:05:30 +0900

    2023-03-31

--- mysite/common/views.py
+++ mysite/common/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.shortcuts import render, redirect
 from .forms import signupForm
+from .forms import UserProfileForm
+from django.contrib.auth.forms import PasswordChangeForm
 
 def main(request):
     return render(request, 'common/main.html')
@@ -11,6 +13,9 @@ def main_pop(request):
 
 def board(request):
     return render(request, 'pybo/pybo.html')
+
+def produce(request):
+    return render(request, 'common/produce.html')
 
 def signup(request):
     if request.method == "POST":
@@ -29,8 +34,38 @@ def signup(request):
         form = signupForm()
     return render(request, 'common/signup.html', {'form': form})
 
-def produce(request):
-    return render(request, 'common/produce.html')
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('common:main')
+        else:
+            return redirect('common:profile')
+    else:
+        form = UserProfileForm(instance=request.user)
+        return render(request, 'common/profile.html', {'form': form})
+
+def profile_pw(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            new_password1 = form.cleaned_data.get('new_password1')
+            user = authenticate(password=new_password1)  # 사용자 인증
+            auth_login(request, user)  # 로그인
+            return redirect('common:main')
+        else:
+            return redirect('common:profile_pw')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'common/profile_pw.html', {'form': form})
+
+
+
+
+
+
 
 
 
